708e1b825cf29ff041aa1bb6886c0b644f02f342
Valentin Fedyakov <dagot32167@gmail.com>
2019-12-04 08:58:26 +0000

    init

--- src/index.js
+++ src/index.js
@@ -2,27 +2,214 @@
 import { LitElement, html, css } from "lit-element";
 
 // Extend the LitElement base class
-export default class MyElement extends LitElement {
+export default class LmRuButton extends LitElement {
   static get properties() {
     return {
-      name: { type: String }
+      buttonstyle: { type: String },
+      buttonsize: { type: String, reflect: true },
+      scalable: { type: Boolean, reflect: true }
     };
   }
 
   constructor() {
     super();
-    this.name = "World";
+    this.buttonstyle = "base";
+    this.buttonsize = "";
+    this.scalable = false;
   }
 
   static get styles() {
     return css`
-      .btn {
-        font-size: 200%;
-      }
-      `;
+        :host {
+          --button-color: inherit;
+          --button-hover-color: var(--button-color);
+          --button-bg: transparent;
+          --button-hover-bg: var(--button-bg);
+          --button-border-color: transparent;
+          --button-outline: blueviolet;
+          --button-border-radius: 4px;
+          --button-active-scale: 0.98;
+
+          position: relative;
+
+          color: var(--button-color);
+          background-color: var(--button-bg);
+
+          border-radius: var(--button-border-radius);
+        }
+
+        :host([scalable]) {
+          display: inline-flex;
+          background-color: transparent;
+        }
+
+        .button-scalable-bg {
+          --scale: 1;
+
+          position: absolute;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          z-index: 0;
+
+          display: block;
+
+          background-color: var(--button-bg);
+          border-radius: var(--button-border-radius);
+
+          transform: scale(var(--scale));
+          transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out, transform 0.2s ease-in;
+
+          will-change: transform;
+        }
+
+        ::slotted(button),
+        ::slotted(a) {
+          z-index: 0;
+          display: inline-flex;
+          align-items: center;
+          justify-content: center;
+          box-sizing: border-box;
+          padding: 0;
+          color: var(--button-color) !important;
+          font-weight: 400;
+          font-size: 1rem;
+          font-family: inherit;
+          line-height: 1em;
+          text-decoration: none;
+          background-color: var(--button-bg) !important;
+          border: 1px solid var(--button-border-color);
+          border-radius: var(--button-border-radius);
+        }
+
+        ::slotted(*) {
+          -webkit-appearance: none !important;
+          -moz-appearance: none !important;
+          appearance: none !important;
+          user-select: none;
+          -webkit-tap-highlight-color: transparent;
+          cursor: pointer;
+
+          transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
+        }
+
+        :host([scalable]) ::slotted(button),
+        :host([scalable]) ::slotted(a) {
+          background-color: transparent !important;
+        }
+
+        @media (hover: hover) and (min-width: 1024px),
+          (min-width: 0\0), (min--moz-device-pixel-ratio: 0) {
+            :host(:not([scalable]):hover),
+            :host(:not([scalable]):hover) ::slotted(button),
+            :host(:not([scalable]):hover) ::slotted(a),
+            :host([scalable]:hover) .button-scalable-bg {
+              background-color: var(--button-hover-bg) !important;
+            }
+
+            :host(:hover),
+            :host(:hover) ::slotted(button),
+            :host(:hover) ::slotted(a) {
+             color: var(--button-hover-color) !important;
+          } 
+        }
+
+        :host(:not([scalable]):active),
+        :host(:not([scalable]):active) ::slotted(button),
+        :host(:not([scalable]):active) ::slotted(a), 
+        :host([scalable]:active) .button-scalable-bg {
+          background-color: var(--button-hover-bg) !important;
+        }
+
+        :host(:active),
+        :host(:active) ::slotted(button),
+        :host(:active) ::slotted(a) {
+          color: var(--button-hover-color) !important;
+        }
+
+        :host([scalable]:active) .button-scalable-bg {
+          --scale: var(--button-active-scale);
+        }
+
+        ::slotted(button:focus),
+        ::slotted(a:focus) {
+          outline: none;
+        }
+
+        ::slotted(button:focus-visible),
+        ::slotted(a:focus-visible) {
+          box-shadow: 0 0 0 3px var(--button-outline);
+        }
+
+        ::slotted(button.focus-visible),
+        ::slotted(a.focus-visible) {
+          box-shadow: 0 0 0 3px var(--button-outline);
+        }
+
+        :host([buttonstyle='primary']) {
+          --button-color: #fff;
+          --button-bg: #484e5c;
+          --button-hover-bg: #585f71;
+        }
+
+        :host([buttonstyle='secondary']) {
+          --button-color: rgb(72, 78, 92);
+          --button-bg: #ffffff;
+          --button-hover-bg: #f7f7f7;
+        }
+
+        :host([buttonstyle='text']) {
+          --button-color: rgb(72, 78, 92);
+          --button-hover-color: #e6615e;
+        }
+
+        .button.m::slotted(button),
+        .button.m::slotted(a) {
+          padding: 16px 40px;
+          font-size: 16px;
+          line-height: 19px;
+        }
+
+        .button.l::slotted(button),
+        .button.l::slotted(a) {
+          padding: 24px 76px;
+          font-size: 18px;
+          line-height: 21px;
+        }
+
+        .button.l::slotted(button),
+        .button.l::slotted(a) {
+          padding: 24px 76px;
+          font-size: 18px;
+          line-height: 21px;
+        }
+
+        .button.fullwidth::slotted(button),
+        .button.fullwidth::slotted(a) {
+          width: 100%;
+          padding: 16px 24px;
+          font-size: 16px;
+          line-height: 19px;
+        }
+
+        .button.container::slotted(button),
+        .button.container::slotted(a) {
+          width: 100%;
+          height: 100%;
+        }
+
+    `;
   }
 
   render() {
-    return html`<button class="btn">Hello ${this.name}!</button>`;
+    return html`
+      ${this.scalable
+        ? html`
+            <div class="button-scalable-bg"></div>
+          `
+        : ""}
+      <slot class="button ${this.buttonstyle} ${this.buttonsize}"></slot>
+    `;
   }
 }
--- src/index.stories.js
+++ src/index.stories.js
@@ -1,8 +1,81 @@
-
 import { html } from "lit-html";
 
-export const story1 = () => html`
-<custom-element name="you">
-</custom-element>
+export const example = () => html`
+  <section>
+    <lmui-button buttonstyle="primary">
+      <button>Привет, я кнопка</button>
+    </lmui-button>
+  </section>
 
+  <section>
+    <lmui-button buttonstyle="secondary">
+      <button>Привет, я кнопка</button>
+    </lmui-button>
+  </section>
+
+  <section>
+    <lmui-button buttonstyle="primary" buttonsize="m">
+      <button>Привет, я кнопка</button>
+    </lmui-button>
+  </section>
+
+  <section>
+    <lmui-button buttonstyle="primary" buttonsize="l">
+      <button>Привет, я кнопка</button>
+    </lmui-button>
+  </section>
+
+  <section>
+    <lmui-button buttonstyle="primary" buttonsize="fullwidth">
+      <button>Привет, я кнопка</button>
+    </lmui-button>
+  </section>
+
+  <section>
+    <lmui-button buttonstyle="secondary" scalable buttonsize="fullwidth">
+      <button>Привет, я кнопка</button>
+    </lmui-button>
+  </section>
+  <section>
+    <lmui-button buttonsize="fullwidth">
+      <a href="https://example.com">Привет, я ссылка</a>
+    </lmui-button>
+  </section>
+  <section>
+    <lmui-button buttonstyle="secondary" scalable buttonsize="fullwidth">
+      <a href="https://example.com">Привет, я сслыка</a>
+    </lmui-button>
+  </section>
+
+  <section>
+    <lmui-button buttonstyle="text" scalable buttonsize="fullwidth">
+      <button>Привет, я текстовая кнопка</button>
+    </lmui-button>
+
+    <lmui-button buttonstyle="primary" scalable buttonsize="fullwidth">
+      <button>Привет, я уменьшающаяся при нажатии кнопка</button>
+    </lmui-button>
+  </section>
+`;
+
+export const withExtenralStyles = () => html`
+  <style>
+    button,
+    a {
+      color: #66c05d;
+      -webkit-transition: all 0.2s ease;
+      transition: all 0.2s ease;
+    }
+    button:focus,
+    button:hover {
+      color: #4ba842;
+    }
+  </style>
+  <section>
+    <lmui-button buttonstyle="secondary" buttonsize="m">
+      <button>
+        Привет. Я должна быть кнопкой на белом фоне с серым ховером
+      </button>
+    </lmui-button>
+  </section>
 `;
